28022c92c748f3d4d999b52857d680b948d24fdc
Ana Paula Carrillo <anapaulacarrillo00@gmail.com>
2026-03-27 23:37:19 -0600

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -40,26 +40,4 @@ CREATE TABLE activo (
     FOREIGN KEY (id_campus) REFERENCES campus(id_campus)
 ) ENGINE=InnoDB;
 
--- Datos básicos válidos
-INSERT INTO campus(nombre) VALUES 
-('Northridge'),
-('Riverside'),
-('San Marcos'),
-('TOOF');
 
-INSERT INTO categoria_activo(nombre) VALUES 
-('Hardware'),
-('Consumable'),
-('Tools'),
-('Supplies');
-
-INSERT INTO departamento(nombre) VALUES 
-('WELD'),
-('HVAC'),
-('AUTO');
-
-INSERT INTO ubicacion (nombre, id_campus) VALUES
-('Container 11B', 1),
-('Room 4150', 2),
-('Shipping Container', 3),
-('Classroom', 4);
