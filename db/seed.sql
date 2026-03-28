640177657cd4c09c7adeb07727d10196b923459c
Ana Paula Carrillo <anapaulacarrillo00@gmail.com>
2026-03-27 23:40:08 -0600

    Update seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -21,3 +21,18 @@ INSERT INTO ubicacion (nombre, id_campus) VALUES
 ('Room 4150', 2),
 ('Shipping Container', 3),
 ('Classroom', 4);
+
+INSERT INTO activo (
+  serial_number,
+  descripcion,
+  estado,
+  condicion,
+  purchase_date,
+  id_categoria,
+  id_departamento,
+  id_ubicacion,
+  id_campus
+)
+VALUES
+('TX611714', 'Emissions Testing Equipment', 'In Use', 'Good', '2022-05-02', 1, 3, 1, 1),
+('0924E03657', 'Heat Pump', 'In Use', 'Good', '2022-07-10', 4, 2, 2, 2);
